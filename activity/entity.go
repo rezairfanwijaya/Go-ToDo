3579e1ac0318c576920b8875db22d7005a3bd196
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-24 23:48:47 +0700

    update: typo

--- activity/entity.go
+++ activity/entity.go
@@ -3,7 +3,7 @@ package activity
 import "time"
 
 type Activity struct {
-	ID         int       `json:"id,omitempty" gorm:"primaryKey" `
+	ID         int       `json:"id,omitempty" gorm:"primaryKey"`
 	Title      string    `json:"title,omitempty"`
 	Email      string    `json:"email,omitempty"`
 	CreatredAt time.Time `json:"updatedAt,omitempty"`
