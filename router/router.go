235bc42919b103a981d5f03879a140ea58f5e835
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-24 11:26:54 +0700

    add: endpoint create activity

---
+++ router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"gotodo/activity"
+	"gotodo/handler"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func NewRoute(db *gorm.DB, r *gin.Engine) {
+	// repo activity
+	activityRepo := activity.NewActivityRespository(db)
+	// service activity
+	activityService := activity.NewActivityService(activityRepo)
+	// handler activity
+	activityHandler := handler.NewActivityHandler(activityService)
+
+	// endpoints
+	r.POST("/activity-groups", activityHandler.CreateActivity)
+}
