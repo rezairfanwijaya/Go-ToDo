aec846006531ce53a82011da7d84fd6b17cc8b6b
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-24 11:27:29 +0700

    update: typo

--- activity/entity.go
+++ activity/entity.go
@@ -6,6 +6,6 @@ type Activity struct {
 	ID         int       `json:"id" gorm:"primaryKey"`
 	Title      string    `json:"title"`
 	Email      string    `json:"email"`
-	CreatredAt time.Time `json:"creatredAt"`
-	UpdatedAt  time.Time `json:"updatedAt"`
+	CreatredAt time.Time `json:"updatedAt"`
+	UpdatedAt  time.Time `json:"createdAt"`
 }
