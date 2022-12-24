8b8054440a3502c3d44a6ed5e738ea4a1c00177f
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-24 22:26:16 +0700

    add: struct input for update activity

--- activity/input.go
+++ activity/input.go
@@ -4,3 +4,7 @@ type ActivityCreateInput struct {
 	Title string `json:"title" binding:"required"`
 	Email string `json:"email" binding:"required,email"`
 }
+
+type ActivityUpdateInput struct {
+	Title string `json:"title" binding:"required"`
+}
