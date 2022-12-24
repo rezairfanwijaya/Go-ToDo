d552a5b35e5577cbd6c0b80d30e369b2a147ce37
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-24 21:58:42 +0700

    update: omitempty on struct activity

--- activity/entity.go
+++ activity/entity.go
@@ -3,9 +3,9 @@ package activity
 import "time"
 
 type Activity struct {
-	ID         int       `json:"id" gorm:"primaryKey"`
-	Title      string    `json:"title"`
-	Email      string    `json:"email"`
-	CreatredAt time.Time `json:"updatedAt"`
-	UpdatedAt  time.Time `json:"createdAt"`
+	ID         int       `json:"id,omitempty" gorm:"primaryKey" `
+	Title      string    `json:"title,omitempty"`
+	Email      string    `json:"email,omitempty"`
+	CreatredAt time.Time `json:"updatedAt,omitempty"`
+	UpdatedAt  time.Time `json:"createdAt,omitempty"`
 }
