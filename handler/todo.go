a90f93a9b3f13c6a2bb5bf897fbe9894d21ab30a
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-25 00:03:36 +0700

    add: handler to get todo by id

--- handler/todo.go
+++ handler/todo.go
@@ -4,6 +4,7 @@ import (
 	"gotodo/todo"
 	"gotodo/utils"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -58,4 +59,49 @@ func (h *todoHandler) CreateTodo(c *gin.Context) {
 	)
 
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *todoHandler) GetTodoByID(c *gin.Context) {
+	// get id from param
+	idParam := c.Param("id")
+
+	// convert to int
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		response := utils.ResponseAPI(
+			http.StatusText(http.StatusBadRequest),
+			nil,
+			"id must be int",
+			true,
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// call service
+	todoByID, err := h.serviceTodo.GetTodoById(id)
+	if err != nil {
+		response := utils.ResponseAPI(
+			http.StatusText(http.StatusBadRequest),
+			nil,
+			err.Error(),
+			true,
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	todoFormatted := todo.FormatterTodo(todoByID)
+
+	response := utils.ResponseAPI(
+		"Success",
+		todoFormatted,
+		"Success",
+		false,
+	)
+
+	c.JSON(http.StatusOK, response)
+
 }
