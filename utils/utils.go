25405ccb11e2fe6110747e56a8c089cde2fd7564
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-24 11:26:33 +0700

    add: format response api and error binding

---
+++ utils/utils.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type responseAPISuccess struct {
+	Status  string      `json:"status"`
+	Message interface{} `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type responseAPIFailed struct {
+	Status  string      `json:"status"`
+	Message interface{} `json:"message"`
+}
+
+func ResponseAPI(status string, data, message interface{}, IsError bool) interface{} {
+	if IsError {
+		return responseAPIFailed{
+			Status:  status,
+			Message: message,
+		}
+	}
+
+	return responseAPISuccess{
+		Status:  status,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func ErrorBinding(err error) []string {
+	var myerr []string
+
+	for _, e := range err.(validator.ValidationErrors) {
+		errMessage := fmt.Sprintf("error on filed: %v, condition: %v", e.Field(), e.ActualTag())
+		myerr = append(myerr, errMessage)
+	}
+
+	return myerr
+}
