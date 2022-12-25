8af4db1b45708c27397ecbd7f36ce3cfd1d8c0ce
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-25 09:34:49 +0700

    add: struct input for update todo

--- todo/input.go
+++ todo/input.go
@@ -5,3 +5,10 @@ type TodoCreateInput struct {
 	ActivityID int    `json:"activity_group_id" binding:"required"`
 	IsActive   bool   `json:"is_active" binding:"required"`
 }
+
+type TodoUpdateInput struct {
+	Title    string `json:"title" binding:"required"`
+	Priority string `json:"priority" binding:"required"`
+	IsActive bool   `json:"is_active" binding:"required"`
+	Status   string `json:"status" binding:"required"`
+}
